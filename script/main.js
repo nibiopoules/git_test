6dbfe5d06513614b5609c0d2ad9e25231164c2a1
nibs <gazou.tommy@gmail.com>
2021-03-17 18:55:26 -0400

    Add a second picture and a picture switcher
    Add a second picture in images/ to use in the new script in main.js to
    switch picture on click.

--- script/main.js
+++ script/main.js
@@ -3,3 +3,13 @@ myHeading.textContent = 'Hello World!';
 /*
 Everything in this is a comment.
  */
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/odin-logo.svg') {
+    myImage.setAttribute('src', 'images/odin2.jpg');
+    } else {
+      myImage.setAttribute('src', 'images/odin-logo.svg');
+    }
+}
