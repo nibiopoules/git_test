2a7c7224cfd6c76ec85e2b72062673ffbe717db0
nibs <gazou.tommy@gmail.com>
2021-03-17 22:37:22 -0400

    Add a button and make it change username

--- script/main.js
+++ script/main.js
@@ -1,15 +1,39 @@
-const myHeading = document.querySelector('h1'); // This is a comment.
-myHeading.textContent = 'Hello World!';
+//const myHeading = document.querySelector("h1"); // This is a comment.
+//myHeading.textContent = "Hello World!";
 /*
 Everything in this is a comment.
  */
-let myImage = document.querySelector('img');
+let myImage = document.querySelector("img");
 
-myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/odin-logo.svg') {
-    myImage.setAttribute('src', 'images/odin2.jpg');
-    } else {
-      myImage.setAttribute('src', 'images/odin-logo.svg');
-    }
+myImage.onclick = function () {
+  let mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/odin-logo.svg") {
+    myImage.setAttribute("src", "images/odin2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/odin-logo.svg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  let myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = "Mozilla is cool, " + myName;
+  }
 }
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem("name");
+  myHeading.textContent = "Mozilla is cool, " + storedName;
+}
+
+myButton.onclick = function () {
+  setUserName();
+};
